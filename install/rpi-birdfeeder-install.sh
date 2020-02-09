ffef957f9e4fcb9fca290d06d038ce72bb8fe56e
John Hawkins <littlehawk93@users.noreply.github.com>
2020-02-09 15:59:03 -0600

    updated bash script to fix bugs in saving downloaded files

--- install/rpi-birdfeeder-install.sh
+++ install/rpi-birdfeeder-install.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-wget --directory-prefix=/usr/bin --output-document=rpi-birdfeeder https://raw.githubusercontent.com/littlehawk93/rpi-birdfeeder/install/install/rpi-birdfeeder
+wget --output-document="/usr/bin/rpi-birdfeeder" https://raw.githubusercontent.com/littlehawk93/rpi-birdfeeder/install/install/rpi-birdfeeder
 
-mkdir /etc/rpi-birdfeeder
-
-if [ -f "/etc/rpi-birdfeeder/config.json"]; then
-    echo "Config file already exists"
+if [ -f "/etc/rpi-birdfeeder" ] && [ -d "/etc/rpi-birdfeeder"]; then
+    echo "Config folder already exists"
 else
-    wget --directory-prefix=/etc/rpi-birdfeeder --output-document=config.json https://raw.githubusercontent.com/littlehawk93/rpi-birdfeeder/install/install/default-config.json
+    mkdir /etc/rpi-birdfeeder
 fi
 
-wget --directory-prefix=/etc/systemd/system --output-document=feeder.powermon.service https://raw.githubusercontent.com/littlehawk93/rpi-birdfeeder/install/install/feeder.powermon.service
-wget --directory-prefix=/etc/systemd/system --output-document=feeder.watch.service https://raw.githubusercontent.com/littlehawk93/rpi-birdfeeder/install/install/feeder.watch.service
+if [ -f "/etc/rpi-birdfeeder/config.json" ]; then
+    echo "Config file already exists"
+else
+    wget --output-document="/etc/rpi-birdfeeder/config.json" https://raw.githubusercontent.com/littlehawk93/rpi-birdfeeder/install/install/default-config.json
+fi
+
+wget --output-document="/etc/systemd/system/feeder.powermon.service" https://raw.githubusercontent.com/littlehawk93/rpi-birdfeeder/install/install/feeder.powermon.service
+wget --output-document="/etc/systemd/system/feeder.watch.service" https://raw.githubusercontent.com/littlehawk93/rpi-birdfeeder/install/install/feeder.watch.service
 
 systemctl enable feeder.powermon
 systemctl enable feeder.watch
