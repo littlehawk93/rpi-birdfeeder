13b750fb217098ee1da1429fd07a58bb6a394d05
John Hawkins <littlehawk93@users.noreply.github.com>
2020-02-09 15:07:05 -0600

    removed ping sensor configuration from watch process config

--- proc/watch/model/config.go
+++ proc/watch/model/config.go
@@ -7,17 +7,10 @@ type MotionSensorConfig struct {
 	SignalPin string `mapstructure:"signal"`
 }
 
-// RangeFinderConfig configuration parameters fro the ultrasonic rangefinder sensor
-type RangeFinderConfig struct {
-	EchoPin    string `mapstructure:"echo"`
-	TriggerPin string `mapstructure:"trigger"`
-}
-
 // WatchConfig configuration parameters for the Watch process. Configuration parameters are expected to not frequently change between application launches.
 // The values are provided via a configuration file rather than through the command line
 type WatchConfig struct {
 	MotionSensor              *MotionSensorConfig `mapstructure:"motion_sensor"`
-	RangeFinderSensor         *RangeFinderConfig  `mapstructure:"range_finder"`
 	CameraConfig              *camera.Config      `mapstructure:"camera"`
 	OutputFolder              string              `mapstructure:"output_dir"`
 	MinCaptureIntervalSeconds int                 `mapstructure:"min_capture_interval"`
